081b4aeffd630da4c8b313e9c69a381299b32d3f
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-02-26 15:13:50 +0000

    Deploying to gh-pages from @ Vladyslav-H/goit-js-hw-09@60c5ab7b6d23ba8526f9fceafc724e202c64433a 🚀

---
+++ 01-color-switcher.3e17d6db.js
@@ -0,0 +1,2 @@
+!function(){var t=document.body,e=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");e.addEventListener("click",(function(){e.disabled=!0,d.disabled=!1,timerId=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),d.addEventListener("click",(function(){e.disabled=!1,d.disabled=!0,clearTimeout(timerId)}))}();
+//# sourceMappingURL=01-color-switcher.3e17d6db.js.map
