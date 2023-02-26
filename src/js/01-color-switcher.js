d84ab4d541516b75fdc113d18c813e1c17deb6d9
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-02-26 16:51:42 +0200

    Update 09-mod 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const bodyEl = document.body;
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
+
+btnStartEl.addEventListener('click', onBtnStartClick);
+btnStopEl.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStartEl.disabled = true;
+  btnStopEl.disabled = false;
+}
+function onBtnStopClick() {
+  clearTimeout(timerId);
+  btnStartEl.disabled = false;
+  btnStopEl.disabled = true;
+}
 
