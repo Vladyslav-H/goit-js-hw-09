7ee149c2b885cf7ede7618aac857b12c1616f005
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-03-01 20:00:12 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
+import throttle from 'lodash.throttle';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('form');
+
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', onSubmit);
+
+function onInput() {
+  const input = {
+    delay: formEl.delay.value,
+    step: formEl.step.value,
+    amount: formEl.amount.value,
+  };
+  if (input.delay < 0 || input.step < 0 || input.amount < 0) {
+    Notify.warning(`Please, input valid value`);
+  }
+  localStorage.setItem('key', JSON.stringify(input));
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  const { delay, step, amount } = JSON.parse(localStorage.getItem('key'));
+  let totalDelay = +delay;
+  for (let i = 1; i <= +amount; i++) {
+    createPromise(i, totalDelay).then(fullfill).catch(error);
+    totalDelay += +step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function fullfill({ position, delay }) {
+  Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+}
+function error({ position, delay }) {
+  Notify.failure(`Rejected promise ${position} in ${delay}ms`);
 }
